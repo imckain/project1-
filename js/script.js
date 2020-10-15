9fa0b1d56b852b3cf318130ca6d7c4d5ae3d0a1e
Ian McKain <ianmmckain@gmail.com>
2020-10-15 18:03:23 -0400

    added html, css, and started on API

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
+// Materialize Init
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+// Constants
 const key = CONFIG.recipeAPIKey;
 const id = CONFIG.recipeID
 const url = 'https://api.edamam.com/search'
 
-$.ajax(url + '?q=chicken&app_id=' + id + '&app_key=' + key)
-.then(data => console.log(data))
-.then(error => console.log(error))
+// Variables
+let recipeData, recipeDetail;
+var instance = M.Sidenav.getInstance(elem);
+
+// Cached Element Refernces
+const $recipes = $('');
+const $featuredRecipes = $('#featured-recipe');
+
+
+// Event Listeners
+// Functions
+
+function handleGetData(event) {
+    event.preventDefault();
+    $.ajax(url + '?q=chicken&app_id=' + id + '&app_key=' + key)
+    .then(function (data) {
+        recipeData = data
+        render()
+    }, function(error) {
+        console.log('Bad Request ', error);
+    })
+};
+
+function render() {
+    $featuredRecipes.text()
+}
