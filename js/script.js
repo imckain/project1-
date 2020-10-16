85b630f1a24e984a3eae6a9f536d01127511e8e1
Ian McKain <ianmmckain@gmail.com>
2020-10-15 23:22:14 -0400

    got api working

--- js/script.js
+++ js/script.js
@@ -1,36 +1,56 @@
 // Materialize Init
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
-  });
-
+});
 // Constants
-const key = CONFIG.recipeAPIKey;
-const id = CONFIG.recipeID
-const url = 'https://api.edamam.com/search'
+const KEY = CONFIG.recipeAPIKey;
+const ID = '4be09ebb'
+const BASE_URL = 'https://api.edamam.com/search';
 
 // Variables
 let recipeData, recipeDetail;
-var instance = M.Sidenav.getInstance(elem);
+// var instance = M.Sidenav.getInstance(elem);
 
 // Cached Element Refernces
-const $recipes = $('');
-const $featuredRecipes = $('#featured-recipe');
-
+const $recipeEl = $('#recipe');
+const $featuredRecipeEl = $('#featured-recipe');
+const $image = $('#image');
+const $label = $('#label');
+const $yield = $('#yield');
+const $dietLabels = $('#diet-labels');
+const $ingredientLines = $('#ingredient-lines');
+const $urlLink = $('#url');
 
 // Event Listeners
+$recipeEl.on('click', '')
 // Functions
+init();
+function init() {
+    handleGetData();
+}
 
-function handleGetData(event) {
-    event.preventDefault();
-    $.ajax(url + '?q=chicken&app_id=' + id + '&app_key=' + key)
-    .then(function (data) {
-        recipeData = data
-        render()
-    }, function(error) {
-        console.log('Bad Request ', error);
-    })
+function handleGetData() {
+    $.ajax(BASE_URL + '?q=chicken&app_id=' + ID + '&app_key=' + KEY)
+        .then(function (data) {
+            recipeData = data;
+            render()
+        }, function (error) {
+            console.log('Bad Request ', error);
+        })
 };
 
+// function
+
 function render() {
-    $featuredRecipes.text()
-}
+    $image.attr({
+        src: recipeData.hits[0].recipe.image,
+        alt: recipeData.hits[0].recipe.label
+    });
+    $label.text(recipeData.hits[0].recipe.label);
+    $yield.text(recipeData.hits[0].recipe.yield);
+    $dietLabels.text(recipeData.hits[0].recipe.dietLabels);
+    $ingredientLines.text(recipeData.hits[0].recipe.ingredientLines);
+    $urlLink.attr({
+        src: recipeData.hits[0].recipe.url,
+    });
+};
